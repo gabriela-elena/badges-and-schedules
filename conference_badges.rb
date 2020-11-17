5a9dcb9e49c08e6762cbbe6f9eb6c87bf8a3dc87
Gabriela Ruvalcaba <gabrielaelenadesigns@gmail.com>
2020-11-17 14:08:20 -0800

    commit tree 7e66ac79ae34a32f887fa1299bc64ba30c1ddddb

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+  end
+  
+  def batch_badge_creator(attendees)
+    new_array = []
+    attendees.each do |name|
+      new_array.push("Hello, my name is #{name}.")
+    end
+    return new_array
+  end
+
+def assign_rooms(attendees)
+    new_array = []
+    counter = 1
+    attendees.each do | name |
+    new_array.push  ("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter+=1
+    end
+    return new_array
+end
+
+def printer(attendees)
+    resultOne = batch_badge_creator(attendees)
+    resultOne.each do |x|
+      puts x 
+    end
+    result = assign_rooms(attendees)
+    result.each do |x|
+      puts x 
+    end
+  end
